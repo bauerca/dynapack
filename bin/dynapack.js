1d974b733894527f5214d2af94bc0af5d1cd9a71
Carl Bauer <carl.a.bauer@gmail.com>
2014-08-27 00:09:22 -0700

    Handle multiple entries with binary

--- bin/dynapack.js
+++ bin/dynapack.js
@@ -3,33 +3,42 @@
 var argv = require('minimist')(process.argv.slice(2));
 var dynapack = require('../');
 var path = require('path');
+var cwd = process.cwd();
 
-var entry = path.join(process.cwd(), argv._[0]);
+var entries = {};
+argv._.forEach(function(entry) {
+  entries[entry] = path.join(cwd, entry);
+});
+
 var opts = {
-  output: path.join(path.dirname(entry), 'chunks')
+  output: path.join(cwd, argv.o || 'dynapack-chunks')
 };
 
-var packer = dynapack(entry, opts);
+var packer = dynapack(entries, opts);
 packer.run(function() {
   packer.write(function(err, entryInfo) {
     if (err) throw err;
 
     console.log(
+      '\n' +
       'Entry info\n' +
       '----------\n\n' +
-      'Each key is a given entry point. Each value is an ' +
-      'array of scripts that should be downloaded by the ' +
-      'associated entry point html page.\n\n' +
-      JSON.stringify(entryInfo, null, 2)
+      'Each key is a given entry point. Each value is an\n' +
+      'array of scripts that should be downloaded by the\n' +
+      'associated entry point html page (assuming you serve\n' +
+      '.js files on the root path).\n\n' +
+      JSON.stringify(entryInfo, null, 2) +
+      '\n'
     );
 
     console.log('Like this:\n');
 
     for (var entry in entryInfo) {
-      console.log(entry + ':');
+      console.log(entry + ':\n');
       entryInfo[entry].forEach(function(src) {
         console.log('  <script src="' + src + '"></script>');
       });
+      console.log('');
     }
   });
 });
