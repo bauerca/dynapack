c2f2e3d3facedd4bfedc8971ed38ebcf66ebc6a4
Carl Bauer <carl.a.bauer@gmail.com>
2014-08-26 23:49:41 -0700

    Update binary to print useful info

--- bin/dynapack.js
+++ bin/dynapack.js
@@ -11,5 +11,25 @@ var opts = {
 
 var packer = dynapack(entry, opts);
 packer.run(function() {
-  packer.write();
+  packer.write(function(err, entryInfo) {
+    if (err) throw err;
+
+    console.log(
+      'Entry info\n' +
+      '----------\n\n' +
+      'Each key is a given entry point. Each value is an ' +
+      'array of scripts that should be downloaded by the ' +
+      'associated entry point html page.\n\n' +
+      JSON.stringify(entryInfo, null, 2)
+    );
+
+    console.log('Like this:\n');
+
+    for (var entry in entryInfo) {
+      console.log(entry + ':');
+      entryInfo[entry].forEach(function(src) {
+        console.log('  <script src="' + src + '"></script>');
+      });
+    }
+  });
 });
